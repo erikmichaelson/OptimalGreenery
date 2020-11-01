91b1186922242a147d76af214a526fa8eb828185
erikmichaelson <47643317+erikmichaelson@users.noreply.github.com>
2020-11-01 16:09:55 -0600

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+#Towards Optimal Greenery
+###How do home prices respond to ambient park levels?
+This repository represents a research project into the effects
+of park proximity, tree density and home prices, and various other
+factors that could affect their interactions.
---
+++ dataSynthesis/shpTest.py
@@ -0,0 +1,13 @@
+import shapefile as shp
+import matplotlib.pyplot as plt
+
+listx=[]
+listy=[]
+sf = shp.Reader("parks/shp_bdry_admin_boundary_data/bdry_park.shp")
+
+plt.figure()
+for shape in sf.shapeRecords():
+    x = [i[0] for i in shape.shape.points[:]]
+    y = [i[1] for i in shape.shape.points[:]]
+    plt.plot(x,y, color="green", fillstyle="full")
+plt.show()
