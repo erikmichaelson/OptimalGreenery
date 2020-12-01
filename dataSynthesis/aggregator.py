7c2bf1c6bfa0fb2d22a978f52562ed2334118e6c
erikmichaelson <47643317+erikmichaelson@users.noreply.github.com>
2020-12-01 12:27:42 -0600

    started modulizing synthesis code and merging in aggregator

---
+++ dataSynthesis/aggregator.py
@@ -0,0 +1,21 @@
+#from census.py import 
+import pandas as pd
+
+'''	stitch together variables from geo-datasets
+	parameters:
+		rasters: raster datasets to pull from
+			type: .tif files
+		APIs: external APIs holding data repositories
+			type: URLs
+
+	returns:
+		
+'''
+def genBatch(size):
+	points = sampler.randPoints(size)
+
+	pass
+	
+
+def mergeBatches(batches):
+	pass
