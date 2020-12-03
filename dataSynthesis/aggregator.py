35fd5734458a20b7c19e08ccfe71082a5d08d97a
erikmichaelson <47643317+erikmichaelson@users.noreply.github.com>
2020-12-03 15:33:33 -0600

    got aggregator split into functions
    Former-commit-id: e73f1f9d1b94a0ff58f30effeccaf573002d8521

--- dataSynthesis/aggregator.py
+++ dataSynthesis/aggregator.py
@@ -7,6 +7,25 @@ import random as rnd
 import matplotlib.pyplot as plt
 
 def genPoints(num, mask_adr):
+	"""
+  	Generate random points inside of a given region 
+
+	Uses python's random functions and shapely's Point object
+	to find random points that lie within a shapefile mask.
+
+    Parameters
+    ----------
+    num : int
+        the number of points to generate
+    mask_adr : str
+        address to a shapefile where the first geometry will be used as a mask
+
+    Returns
+    -------
+    [(int, int)]
+        a list of points (int, int) tuples
+
+    """
 	mask = gpd.read_file('../data/MSP/mask/CountyMask.shp')
 	bnds = mask['geometry'].total_bounds
 	print(bnds)
@@ -25,50 +44,73 @@ def genPoints(num, mask_adr):
 			print('outside the TC')
 		points.append(base)
 
+	for p in points:
+		assert bnds[0] <= base0 <= bnds[2]
+		assert bnds[1] <= base1 <= bnds[3]
+
+	savePointsToFile(points, 'points')
+
 	return points
 
 
 
-def avgGrndCover(points):
+def avgGrndCover(points, rast_adr):
 	rast = rio.open('../data/MSP/trees/tcma_clip.tif', masked=False)
 
 	bnds = rast.bounds
+	print(bnds)
 
 	band1 = rast.read(1)
 	print(band1)
 	gridpoints = []
-	results = []
-
-	for i in range(30):
-		'''
-		point0 = rnd.randrange(base[0]-100, base[0]+100, 2)
-		point1 = rnd.randrange(base[1]-100, base[1]+100, 2)
-		'''
-		point0 = base0 - i*10 + 150
-		for j in range(30):
-			point1 = base1 - j*10 + 150
-			'''
-			cmd=f'rio sample tcma_clip.tif [{str(point0)},{str(point1)}]'
-			process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-			output, error = process.communicate()
-			'''
-			output = band1[point0, point1]
-			point = (point0+bnds[0], point1+bnds[1])
-			#print(point)
-			gridpoints.append(point)
-			results.append(output)
-
-	print(len(gridpoints))
+	toReturn = []
 	
-	Points = [Point(p) for p in gridpoints]
-	geoPts = gpd.GeoSeries(Points)
+	print(bnds[2]-bnds[0], len(band1[0]))
+	print(bnds[3]-bnds[1], len(band1))
 
+	enum = [i for i in range(1,13)]
+	print(enum)
+
+	for p in points:
+		base0 = p[0]
+		base1 = p[1]
+		print(p)
+		assert bnds[0] <= base0 <= bnds[2]
+		assert bnds[1] <= base1 <= bnds[3]
+		results = []
+		for i in range(30):
+			gridpoint0 = base0 - bnds[0] - i*10 + 150 
+			for j in range(30):
+				gridpoint1 = base1 - bnds[1] - j*10 + 150
+				a = int(gridpoint0)
+				b = int(gridpoint1)
+				output = band1[b][a]
+				gridpoint = (gridpoint0+bnds[0], gridpoint1+bnds[1])
+				gridpoints.append(gridpoint)
+				results.append(output)
+		cover = Counter(results)
+		print(cover)
+		toReturn.append(cover)
+
+	#savePointsToFile(gridpoints, 'grids')
+
+	rast.close()
+
+	print(len(toReturn))
+	assert len(toReturn) == len(points)
+
+	return toReturn
+	
+
+def savePointsToFile(array, filename):
+	print('here')
+	Points = [Point(p) for p in array]
+	geoPts = gpd.GeoSeries(Points)
 	print(geoPts)
 	geoPts.columns=['geometry']
-	geoPts.to_file('points.shp')
-
-	print(Counter(results))
+	geoPts.to_file(filename+ '.shp')
 
 if __name__ == '__main__':
 	mask = '~/Documents/AdvEconometrics/ResearchProject/code/data/MSP/mask/CountyMask.shp'
 	points = genPoints(10, mask)
+	groundCover = avgGrndCover(points, 'PASS')
