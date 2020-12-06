9b77593123b91a52d93d8384e11ea9a96353329d
erikmichaelson <47643317+erikmichaelson@users.noreply.github.com>
2020-12-06 12:39:48 -0600

    made threading optional, added try/except to request
    Former-commit-id: 9bb45da566382e88ddb7a2afff2dc1ab3d329dab

--- dataSynthesis/census.py
+++ dataSynthesis/census.py
@@ -3,7 +3,10 @@ from concurrent.futures import ThreadPoolExecutor
 import geopandas as gpd
 import pandas as pd
 
-def blockDataAtCoords(points):
+key = 'b9ff9c9d59bd6d7ed76973dc6b5859cc644a46e6'
+url = 'https://api.census.gov/data/2018/acs/acs5'
+
+def blockDataAtCoords(points, threaded):
 	
 	blockfile = gpd.read_file('../data/MSP/census/tl_2019_27_bg.shp')
 	print(blockfile.crs)
@@ -19,55 +22,60 @@ def blockDataAtCoords(points):
 	print(loc)
 
 	print(blocks)
-	key = 'b9ff9c9d59bd6d7ed76973dc6b5859cc644a46e6'
 	og = 'https://api.census.gov/data/2018/acs/acs5?get=B01003_001E&for=block%20group:2&in=state:01%20county:025%20tract:957602&key='+key
-
-	url = 'https://api.census.gov/data/2018/acs/acs5'
 
 	results = []
 
-	with ThreadPoolExecutor(max_workers=10) as executor:
-		for l in executor.map(queryAPI, blocks['FID'], state, county, tract, group):
-			print(l)
+	if(threaded==True):
+		denials = 0
+		with ThreadPoolExecutor(max_workers=50) as executor:
+			for l in executor.map(queryAPI, blocks['FID'], state, county, tract, group):
+				print(l)
+				if(l == None): 
+					denials += 1
+				else:
+					blocks.at[l[0], 'blockpop'] = l[1]
+					blocks.at[l[0],  'avgAge' ] = l[2]
+			print(denials)
 
-	"""
-	for i in range(100):
-		step = int(len(state)/100)
-		for j in range(i*step,i*step+step):
+	else:
+		for i in range(10):
+			step = int(len(state)/100)
+			for j in range(i*step,i*step+step):
+				queryAPI(blocks.at[j, 'FID'], state[j], county[j], tract[j], group[j])
 
-			with ThreadPoolExecutor(max_workers=step) as executor:
-				future = executor.submit(pow, 323, 1235)
-				print(future.result())	
-
-
-
+			print(blocks)
 		print(blocks)
 
-	print(blocks)
-
-	return toReturn
-	"""
+	return blocks
 
 
-def queryAPI(FID, state, county, tract, block):
+def queryAPI(FID, state, county, tract, blockgroup):
 	j = FID
-	print(j, state, county, tract, block)
+	print(j, state, county, tract, blockgroup)
 	loc = 'state:',state,' county:',county,' tract:',tract
-	bgroup = 'block group:'+group
+	bgroup = 'block group:'+blockgroup
 	#print(bgroup)
 	payload = {'get':'B01003_001E,B01002_001E', 'for':bgroup, 'in':loc, 'key':key}
 	full='https://api.census.gov/data/2018/acs/acs5?get=B01003_001E&for=block%20group:*&in=state:27%20county:053&key='+key
-	r = requests.get(url, params=payload)
-	fjk = pd.read_json(r.text, orient='records')
-	row = fjk.iloc[1]
-	print(row[0], row[1])
+	try:
+		r = requests.get(url, params=payload)
+		fjk = pd.read_json(r.text, orient='records')
+		row = fjk.iloc[1]
+	except requests.exceptions.ConnectionError:
+		requests.status_code = "Connection refused"
+		return None
 
-	blocks.at[j, 'blockpop'] = row[0]
-	blocks.at[j,  'avgAge' ] = row[1]
-
-	return 'success'
+	return j, row[0], row[1]
 
 
 if __name__ == '__main__':
 	Points = gpd.read_file('Points.shp')
-	blocks = blockDataAtCoords(Points)
+	blocks0 = blockDataAtCoords(Points, True)
+	print(blocks0)
+	blocks1 = blockDataAtCoords(Points, False)
+	print(blocks1)
+
+	for b0, b1 in zip(blocks0, blocks1):
+		assert b0[-1] == b1[-1]
+		assert b0[-2] == b1[-2]
