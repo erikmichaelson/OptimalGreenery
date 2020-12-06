a7b3b1084c8e5dde9cc4beb94f44ab8d627b50ac
erikmichaelson <47643317+erikmichaelson@users.noreply.github.com>
2020-12-05 19:54:31 -0600

    got a bad version of the code working, changing to append
    Former-commit-id: f855ad3b417e9003d6095e5b1b3c346d8e422fea

--- dataSynthesis/census.py
+++ dataSynthesis/census.py
@@ -1,15 +1,56 @@
 import requests
+import geopandas as gpd
+import pandas as pd
 
-key = 'b9ff9c9d59bd6d7ed76973dc6b5859cc644a46e6'
+def blockDataAtCoords(points):
+	
+	blockfile = gpd.read_file('../data/MSP/census/tl_2019_27_bg.shp')
+	print(blockfile.crs)
+	geoPts = points.set_crs('epsg:26915')
+	geoPts = geoPts.to_crs(blockfile.crs)
+	print(geoPts.crs)
+	blocks = gpd.sjoin(geoPts, blockfile, how='left', op='intersects')
+	state = blocks['STATEFP']
+	county = blocks['COUNTYFP']
+	tract = blocks['TRACTCE']
+	group = blocks['BLKGRPCE']
+	print(group)
 
-mn = '024'
-og = 'https://api.census.gov/data/2018/acs/acs5?get=B01003_001E&for=block%20group:2&in=state:01%20county:025%20tract:957602&key='+key
+	print(blocks)
+	key = 'b9ff9c9d59bd6d7ed76973dc6b5859cc644a46e6'
+	og = 'https://api.census.gov/data/2018/acs/acs5?get=B01003_001E&for=block%20group:2&in=state:01%20county:025%20tract:957602&key='+key
 
-url = 'https://api.census.gov/data/2018/acs/acs5'
-payload = {'get':'B00001_001E', 'for':'block group:2', 'key':key}
-full='https://api.census.gov/data/2018/acs/acs5?get=B01003_001E&for=block%20group:*&in=state:27%20county:053&key='+key
-print(full)
-r = requests.get(full)
+	url = 'https://api.census.gov/data/2018/acs/acs5'
 
-print(r.json)
-print(r.text)
+	results = []
+	toReturn = gpd.GeoDataFrame(columns=['geometry','B01003_001E','B01002_001E','state','county','tract','block group'])
+	for i in range(100):
+		step = int(len(state)/100)
+	#	print(step)
+		for j in range(i*step,i*step+step):
+			print(j)
+			loc = 'state:',state[j],' county:',county[j],' tract:',tract[j]
+			bgroup = 'block group:'+group[j]
+			#print(bgroup)
+			payload = {'get':'B01003_001E,B01002_001E', 'for':bgroup, 'in':loc, 'key':key}
+			full='https://api.census.gov/data/2018/acs/acs5?get=B01003_001E&for=block%20group:*&in=state:27%20county:053&key='+key
+			r = requests.get(url, params=payload)
+			fjk = pd.read_json(r.text, orient='records')
+			fjk.columns = fjk.iloc[0]
+			fjk.drop(fjk.index[0])
+			print(geoPts.iloc[j])
+			fjk['geometry'] = geoPts.iloc[j]
+			print(fjk)
+			toReturn = toReturn.append(fjk.iloc[1], ignore_index=True)
+
+		print(toReturn)
+
+	print(toReturn)
+
+	return toReturn
+
+
+
+if __name__ == '__main__':
+	Points = gpd.read_file('Points.shp')
+	blocks = blockDataAtCoords(Points)
